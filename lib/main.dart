caf7d3c277e9e8a577e572cc845f0e6ab633552c
riteshb-25 <riteshjbhattad@gmail.com>
2020-07-01 11:11:33 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/todo_list.dart';
+
+void main() {
+  runApp(TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'TodoList',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      home: TodoList(),
+    );
+  }
+}
